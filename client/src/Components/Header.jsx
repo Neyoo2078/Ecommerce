14914803709fa99418d7be1d4d53bb21abd73152
adeniyi joseph <neyoo2078@yahoo.com>
2023-03-10 20:38:18 +0100

    new update

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="bg-[#4d4646] text-[#ffff] p-3">
-      <div>header</div>
+      <div>header is here</div>
     </div>
   );
 };
