d35d1f890d85756d660b1e07c697ee612a61ed89
adeniyi joseph <neyoo2078@yahoo.com>
2023-03-11 05:46:49 +0100

    Morning run

---
+++ client/src/Components/Feature.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { data } from '../data';
+
+const Feauture = () => {
+  return <div>Feauture</div>;
+};
+
+export default Feauture;
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Header } from '../Components';
+import { Feature, Header } from '../Components';
 
 const Home = () => {
   return (
     <div>
       <div>
         <Header />
+        <Feature />
       </div>
     </div>
   );
